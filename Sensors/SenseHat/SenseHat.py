f24488e8992e16a7dcf3abf5da2575f92557b7a4
solcmich <michal.solc.it@seznam.cz>
2021-04-10 09:40:27 +0100

    U

--- Sensors/SenseHat/SenseHat.py
+++ Sensors/SenseHat/SenseHat.py
@@ -13,17 +13,17 @@ class CSenseHat:
         return ret
 
     def get_temperature(self):
-        return self._get(self.hat.get_temperature())
+        return self._get(self.hat.get_temperature)
 
     def get_accelerometer(self):
-        return self._get(self.hat.get_accelerometer())
+        return self._get(self.hat.get_accelerometer)
 
     def get_gyroscope(self):
-        return self._get(self.hat.get_gyroscope())
+        return self._get(self.hat.get_gyroscope)
 
     def get_pressure(self):
-        return self._get(self.hat.get_pressure())
+        return self._get(self.hat.get_pressure)
 
     def get_humidity(self):
-        return self._get(self.hat.get_humidity())
+        return self._get(self.hat.get_humidity)
 
