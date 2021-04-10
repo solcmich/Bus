110ce3b9ea477fd29ea6169d8d0971a3f07142cc
solcmich <michal.solc.it@seznam.cz>
2021-04-10 09:39:53 +0100

    U

--- Sensors/SenseHat/SenseHat.py
+++ Sensors/SenseHat/SenseHat.py
@@ -1,29 +1,29 @@
 from sense_hat import SenseHat
 from datetime import datetime
+import pandas as pd
 
 class CSenseHat:
     def __init__(self):
         self.hat = SenseHat()
 
-    @staticmethod
-    def _get(functor):
+    def _get(self, f):
         ret = pd.DataFrame()
         ret.date = [datetime.now()]
-        ret.Value = [functor()]
+        ret.Value = [f()]
         return ret
 
     def get_temperature(self):
-        return _get(self.hat.get_temperature())
+        return self._get(self.hat.get_temperature())
 
     def get_accelerometer(self):
-        return _get(self.hat.get_accelerometer())
+        return self._get(self.hat.get_accelerometer())
 
     def get_gyroscope(self):
-        return _get(self.hat.get_gyroscope())
+        return self._get(self.hat.get_gyroscope())
 
     def get_pressure(self):
-        return _get(self.hat.get_pressure())
+        return self._get(self.hat.get_pressure())
 
     def get_humidity(self):
-        return _get(self.hat.get_humidity())
+        return self._get(self.hat.get_humidity())
 
